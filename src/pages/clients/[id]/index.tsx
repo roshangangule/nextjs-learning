d376452ff770fc60035017e9793f44798d424eaa
roshangangule <roshangangule@gmail.com>
2023-03-14 22:49:41 +0530

    Added other more page routing.

--- src/pages/clients/[id]/index.tsx
+++ src/pages/clients/[id]/index.tsx
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
+
 export default function ClientsProjectPage() {
+    const router = useRouter();
+    function loadProjectHandler() {
+        //router.push("/clients/max/projecta");
+        router.push(
+            {
+                pathname: "/clients/[id]/[clientProjectId]",
+                query: {
+                    id: "max",
+                    clientProjectId: "projecta"
+                }
+            }
+        )
+    }
     return (
         <div>
             <h1>The project of given clients</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+    const clients = [
+        {id: "max", name: "Maxmilion"},
+        {id: "man", name: "Manual"}
+    ];
     return (
         <div>
             <h1>THE CLIENTS PAGE</h1>
+            <ul>
+                {
+                    clients.map((client) => (
+                        <li key={client.id}>
+                            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                        </li>
+                    ))
+                }
+                {/* Different way of setting href */}
+                {
+                    clients.map((client) => (
+                        <li key={client.id}>
+                            <Link 
+                                href={{
+                                    pathname: "/clients/[id]",
+                                    query: {id: client.id}
+                                }}
+                            >{client.name}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
